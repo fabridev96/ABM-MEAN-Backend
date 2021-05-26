ee3318495ddd5109b44700d236c7723032a0a404
Fabrizio Barreto <barretofabrizio@gmail.com>
2021-05-26 08:43:57 -0300

    Añadido CORS para puerto 4200

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 app.set('puerto', process.env.port || 3000);
+
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
 
 app.use(morgan('dev'));
 app.use(express.json());
